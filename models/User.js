c4c7f98187f02293a3f88f8d136adcbb3bec8389
Suleyman Furkan Binay <sfbinay@gmail.com>
2022-07-03 16:01:30 +0300

    bcryptjs installed, mongoose pre hooks and password hash added

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
@@ -65,5 +65,21 @@ const UserSchema = new Schema(
   },
   { collection: "users" } // to set database collection name manually
 );
+
+UserSchema.pre("save", function (next) {
+  // Password Changed
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) next(err);
+      this.password = hash;
+      next();
+    });
+  });
+});
 
 module.exports = mongoose.model("User", UserSchema);
