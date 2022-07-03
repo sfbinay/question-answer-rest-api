995982738057fd09f36b66d7048348c4a619f43a
Suleyman Furkan Binay <sfbinay@gmail.com>
2022-07-03 14:31:52 +0300

    customErrorHandler function controlled with CustomError Class

--- middlewares/errors/customErrorHandler.js
+++ middlewares/errors/customErrorHandler.js
@@ -1,11 +1,22 @@
-// const CustomError = require("../../helpers/error/CustomError");
+const CustomError = require("../../helpers/error/CustomError");
 
 const customErrorHandler = (err, req, res, next) => {
-  // let customError = err;
+  let customError = err;
 
-  res.status(err.status || 500).json({
+  console.log("err.name --> ", typeof err.name);
+
+  if (err.name === "ValidationError") {
+    customError = new CustomError(err.message, 400);
+  }
+  if (err.name === "TypeError") {
+    customError = new CustomError("Custom type error", 400);
+  }
+  if (err.name === "SyntaxError") {
+    customError = new CustomError(err.message, 400);
+  }
+  res.status(customError.status || 500).json({
     success: false,
-    message: err.message || "Internal Server Error",
+    message: customError.message || "Internal Server Error",
   });
 };
 
